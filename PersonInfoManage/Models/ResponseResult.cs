33f2be0d0d1d1aa4f121e2b0bd705c1502604c7a
CodingPanda <lll_tony@qq.com>
2019-08-05 09:57:01 +0800

    [M]修改代码注释

--- PersonInfoManage/Models/ResponseResult.cs
+++ PersonInfoManage/Models/ResponseResult.cs
@@ -7,7 +7,10 @@ namespace PersonInfoManage.Models
 {
     public class ResponseResult
     {
+        //结果
         public int Result { get; set; }
+
+        //结果信息
         public string Message { get; set; }
     }
 }
--- PersonInfoManage/Models/UserModel.cs
+++ PersonInfoManage/Models/UserModel.cs
@@ -7,8 +7,10 @@ namespace PersonInfoManage.Models
 {
     public class UserModel
     {
+        //用户名
         public string name { get; set; }
 
+        //密码
         public string password { get; set; }
     }
 }
